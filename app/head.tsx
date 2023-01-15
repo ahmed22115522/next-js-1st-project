467d64d7699808cfe6acf2feace6c2a3b8aef41d
ahmed rizk <ahmed22115522@gmail.com>
2023-01-15 03:43:36 +0200

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+
+export default function Head() {
+  return (
+    <>
+      <title>Next JS</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
